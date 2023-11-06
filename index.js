10f6691cb0f61806fbcd4e71fd8e72c4443f8aae
ishayankoo001 <ishayankoo@icloud.com>
2023-11-06 17:35:57 +0100

    3.8 done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
 app.listen(3001)
-const persons = [
+let persons = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -24,6 +25,10 @@ const persons = [
       "number": "39-23-6423122"
     }
 ]
+morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :type"))
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
